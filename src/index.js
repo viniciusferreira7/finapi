955e0f82be4453880d730740a9ee0708b593cb48
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-07-04 21:00:21 -0300

    Criado a rota de depositar

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ app.use(express.json())
 const customers = []
 
 function verifyIfExitsAccountCPF(req, res, next) {
-  const {cpf  } = req.headers
+  const { cpf } = req.headers
 
   const customer = customers.find(customer => customer.cpf === cpf)
 
   if (!customer) {
-    return res.status(400).send({error: 'Customers not found'})
+    return res.status(400).send({error: 'Customer not found'})
   }
 
   res.customer = customer
@@ -43,6 +43,22 @@ app.get('/statement', verifyIfExitsAccountCPF, (req, res) => {
  const { customer } = res
 
   return res.status(200).json(customer.statement)
+})
+
+app.post('/deposit', verifyIfExitsAccountCPF, (req, res) => {
+  const { description, amount } = req.body
+  const { customer } = res
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation)
+
+  return res.status(201).send()
 })
 
 app.listen(3000)
